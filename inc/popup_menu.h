3c6d90560f037dd30e4a637c89dbe688b04ce510
Horgeon <horgeon@hotmail.fr>
2016-04-30 22:30:39 +0200

    Modification de l'apparence du menu

--- inc/popup_menu.h
+++ inc/popup_menu.h
@@ -5,9 +5,9 @@
 #include <allegro.h>
 #include <loadpng.h>
 
-#define MENU_BACK_OFFSET 10
-#define MENU_TOP_BAR_OFFSET 3
-#define MENU_LEFT_BAR_OFFSET 3
+#define MENU_BACK_OFFSET 4
+#define MENU_TOP_BAR_OFFSET 1
+#define MENU_LEFT_BAR_OFFSET 1
 #define MENU_FONT_SIZE 14
 #define MENU_LETTER_SPACING_X 2
 #define MENU_LETTER_SPACING_Y 5
