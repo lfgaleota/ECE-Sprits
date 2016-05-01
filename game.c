c5e27c6b8d4cb7b5d8d040a473e6e03ebe40a3d0
Horgeon <horgeon@hotmail.fr>
2016-05-01 17:53:20 +0200

    Correction de bugs suite au renommage

--- game.c
+++ game.c
@@ -11,7 +11,7 @@ void Game_pauseMenu( Level* level ) {
 				"Quitter",
 		};
 
-		choice = Menu_open( "Pause", choices, 4, level->bmps.droidsans_14_mono );
+		choice = PopupMenu_open( "Pause", choices, 4, level->bmps.droidsans_14_mono );
 	} else {
 		char* choices[] = {
 				"Retour",
@@ -19,7 +19,7 @@ void Game_pauseMenu( Level* level ) {
 				"Quitter",
 		};
 
-		choice = Menu_open( "Pause", choices, 3, level->bmps.droidsans_14_mono );
+		choice = PopupMenu_open( "Pause", choices, 3, level->bmps.droidsans_14_mono );
 	}
 
 	switch( choice ) {
