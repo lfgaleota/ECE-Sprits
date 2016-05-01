a4d01ade544333116695f187395418a5425b76c1
Horgeon <horgeon@hotmail.fr>
2016-05-01 16:07:02 +0200

    Ajout de l'enchainement des niveaux

--- main.c
+++ main.c
@@ -1,20 +1,11 @@
 #include <allegro.h>
-#include <loadpng.h>
-#include <jpgalleg.h>
 #include "inc/init.h"
-#include "inc/level.h"
-#include "inc/game.h"
+#include "inc/menu.h"
 
 int main() {
 	Init();
 
-	Level* level = Level_loadStandard( 1 );
-
-	if( level ) {
-		Game_launch( level );
-	} else {
-		allegro_message( "Impossible de charger le niveau 1!" );
-	}
+	Menu_launch();
 
 	return 0;
 }
